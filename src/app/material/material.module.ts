89b51f170a8dbe7a0923f997e7f35e558254a384
coffe <coffe@debian.azharazhar.com>
2020-09-08 02:43:04 +0800

    now using mat-toolbar instead of mat-card

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const material: Array<any> = [
-  CommonModule,
   FlexLayoutModule,
   MatCardModule,
   MatButtonModule,
   MatIconModule,
+  MatToolbarModule,
 ];
 
 @NgModule({
